1f4f0f59cb8ce11ce7ef30d2cb56d7c0aa708940
Lukas Pirl <git@lukas-pirl.de>
2015-10-21 11:29:54 +1300

    added basic layout

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from main import models
+from . import models
 
-admin.site.register(models.Tag)
-admin.site.register(models.Scanner)
-admin.site.register(models.RCCarScan)
+site = admin.site
+
+site.site_header = 'AutoID administration'
+site.site_title = site.site_header
+
+site.register(models.Tag)
+site.register(models.Scanner)
+site.register(models.RCCarScan)
