d9e306174ac48bd7f757f914287b7b9640249834
Lukas Pirl <git@lukas-pirl.de>
2015-10-21 15:05:05 +1300

    added and enabled import/export in admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
+from import_export.admin import ImportExportModelAdmin
+from import_export.resources import ModelResource
+
 from . import models
 
 site = admin.site
@@ -8,6 +11,19 @@ site.site_header = 'AutoID administration'
 site.site_title = 'AutoID administration'
 site.index_title = 'Data administration'
 
-site.register(models.Tag)
-site.register(models.Scanner)
-site.register(models.RCCarScan)
+def register_with_import_export(model_cls, exclude_model_attrs=tuple()):
+
+  class Resource(ModelResource):
+
+    class Meta:
+        model = model_cls
+        exclude = exclude_model_attrs
+
+  class ModelScanAdmin(ImportExportModelAdmin):
+    resource_class = Resource
+
+  site.register(model_cls, ModelScanAdmin)
+
+register_with_import_export(models.Tag)
+register_with_import_export(models.Scanner)
+register_with_import_export(models.RCCarScan, ("id",))
