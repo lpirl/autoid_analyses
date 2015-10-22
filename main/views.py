c84ae9c3fdff5ba42c68fa0b3c1e25dc596cacec
Lukas Pirl <git@lukas-pirl.de>
2015-10-23 11:20:17 +1300

    first working tag popularity chart (absolute numbers)

--- main/views.py
+++ main/views.py
@@ -6,7 +6,17 @@ def index(request):
   return render(request, 'index.html', {})
 
 def tag_popularity(request, scan_cls):
+  scans = scan_cls.objects.select_related('tag').all()
+  tags = [s.tag for s in scans]
+
+  tags_with_scancount = []
+  for tag in set(tags):
+    tags_with_scancount.append((tag, tags.count(tag)))
+  tags_with_scancount = sorted(tags_with_scancount, key=lambda t: t[1],
+                              reverse=True)
+
   context = {
     "name": scan_cls._meta.verbose_name_plural,
+    "tags_with_scancount": tags_with_scancount,
   }
   return render(request, 'tag_popularity.html', context)
