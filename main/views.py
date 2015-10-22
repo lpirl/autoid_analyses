e5e931ebba8bd25002425da709c58b564c830cd9
Lukas Pirl <git@lukas-pirl.de>
2015-10-22 20:13:27 +1300

    added basic view for tag popularity and corresponding URLs

--- main/views.py
+++ main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from main.models import RCCarScan
+
 def index(request):
-  context = {}
-  return render(request, 'index.html', context)
+  return render(request, 'index.html', {})
+
+def tag_popularity(request, scan_cls):
+  context = {
+    "name": scan_cls._meta.verbose_name_plural,
+  }
+  return render(request, 'tag_popularity.html', context)
